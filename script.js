6d790d7dce1b7bf4874185441dce9ad2d262bfe0
Manikanta Itikela <113256332+Manikanta79i@users.noreply.github.com>
2023-04-10 13:27:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
-// your code here
+  if (num <= 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let a = 0;
+    let b = 1;
+    let temp;
+    for (let i = 2; i <= num; i++) {
+      temp = a + b;
+      a = b;
+      b = temp;
+    }
+    return b;
+  }
 }
 
 module.exports = fibonacci;
